ebad888587f45ac97bbb6bbac8b659a76b96ff0d
Ismael Celis <ismaelct@gmail.com>
2010-10-12 16:01:00 +0100

    Using Bundler for deps management

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source :rubygems
+
+gem 'eventmachine'
+gem 'em-http-request'
+gem 'json'
+gem 'activesupport'
+
--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
 
 require 'rubygems'
+require "bundler/setup"
+
 require 'yaml'
 require 'eventmachine'
 require 'active_support/inflector'
@@ -10,9 +12,11 @@ require 'publisher'
 
 EM.run {
   
-  config = YAML.load_file('./config/config.yml')
   
   dispatcher = Dispatcher.new
+  
+  # Configure and register publishers
+  config = YAML.load_file('./config/config.yml')
   
   config[:publishers].each do |publisher_name, settings|    
     dispatcher.register publisher_name, settings
@@ -20,8 +24,8 @@ EM.run {
   
   
   dispatcher.on_message do |publisher_name, message|
-    
     puts "#{publisher_name}: #{message}"
+    
   end
   
   dispatcher.run
