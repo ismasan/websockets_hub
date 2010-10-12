ac8a4edc4ab5ae94e52d7710db7eab3606b3f948
Ismael Celis <ismaelct@gmail.com>
2010-10-12 15:52:19 +0100

    Auto require publisher classes on register

--- app.rb
+++ app.rb
@@ -5,40 +5,8 @@ require 'yaml'
 require 'eventmachine'
 require 'active_support/inflector'
 
+require 'dispatcher'
 require 'publisher'
-require 'twitter_stream'
-
-class Dispatcher
-  
-  def initialize
-    @publishers = {}
-    @callbacks = []
-  end
-  
-  def register(published_klass, settings)
-    require published_klass.to_s
-    @publishers[ActiveSupport::Inflector.classify(published_klass.to_s)] = settings
-  end
-  
-  def run
-    @publishers.each do |klass, settings|
-      pub = ActiveSupport::Inflector.constantize(klass).new(self)
-      pub.setup settings
-      pub.run
-    end
-  end
-  
-  def on_message(&callback)
-    @callbacks << callback
-  end
-  
-  def publish(publisher_name, message)
-    @callbacks.each do |callback|
-      callback.call publisher_name, message
-    end
-  end
-  
-end
 
 EM.run {
   
